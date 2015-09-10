6a99189dcf18d3fc581b972476fc7fbb9cb39ad8
Ajay Kumar <ajayak731@hotmail.com>
2015-09-10 21:47:05 +0530

    Added Customer and CustomerConfiguration class

---
+++ DetailWorkflow/DataLayer/CustomerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using DetailWorkflow.Models;
+
+namespace DetailWorkflow.DataLayer
+{
+    public class CustomerConfiguration : EntityTypeConfiguration<Customer>
+    {
+        public CustomerConfiguration()
+        {
+            Property(c => c.AccountNumber)
+                .HasMaxLength(8)
+                .IsRequired();
+
+            Property(c => c.CompanyName)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            Property(c => c.Address)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            Property(c => c.City)
+                .HasMaxLength(15)
+                .IsRequired();
+
+            Property(c => c.State)
+                .HasMaxLength(2)
+                .IsRequired();
+
+            Property(c => c.ZipCode)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            Property(c => c.Phone)
+                .HasMaxLength(15)
+                .IsOptional();
+        }
+    }
+}
---
+++ DetailWorkflow/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DetailWorkflow.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string AccountNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+    }
+}
