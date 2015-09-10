c0bb43f221339c9d448e0713777b59dd61be7614
Ajay Kumar <ajayak731@hotmail.com>
2015-09-10 21:54:03 +0530

    Added WorkOrder and WorkOrderConfiguration

---
+++ DetailWorkflow/DataLayer/WorkOrderConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using DetailWorkflow.Models;
+
+namespace DetailWorkflow.DataLayer
+{
+    public class WorkOrderConfiguration:EntityTypeConfiguration<WorkOrder>
+    {
+        public WorkOrderConfiguration()
+        {
+            Property(wo => wo.OrderDateTime).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+            Property(wo => wo.Description).HasMaxLength(250).IsOptional();
+
+            Property(wo => wo.Total).HasPrecision(18, 2).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+            Property(wo => wo.CertificationRequirements).HasMaxLength(120).IsOptional();
+        }
+    }
+}
---
+++ DetailWorkflow/Models/WorkOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DetailWorkflow.Models
+{
+    public class WorkOrder
+    {
+        public int WorkOrderId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public DateTime? TargetDateTime { get; set; }
+        public DateTime? DropDeadDateTime { get; set; }
+        public string Description { get; set; }
+        public WorkOrderStatus WorkOrderStatus { get; set; }
+        public decimal Total { get; set; }
+        public string CertificationRequirements { get; set; }
+    }
+
+    public enum WorkOrderStatus
+    {
+        Created = 0,
+        InProgress = 10,
+        Rework = 15,
+        Submitted = 20,
+        Approved = 30,
+        Cancelled = -10,
+        Rejected = -20
+    }
+}
