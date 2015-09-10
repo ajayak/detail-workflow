cfc69163b87fde93ef08f305cb88692b86aa3770
Ajay Kumar <ajayak731@hotmail.com>
2015-09-10 21:42:45 +0530

    Added labour and labour configuration classes

---
+++ DetailWorkflow/DataLayer/LabourConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using DetailWorkflow.Models;
+
+namespace DetailWorkflow.DataLayer
+{
+    public class LabourConfiguration : EntityTypeConfiguration<Labor>
+    {
+        public LabourConfiguration()
+        {
+            Property(p => p.ServiceItemCode)
+                .HasMaxLength(15)
+                .IsRequired()
+                .HasColumnAnnotation("Index",
+                new IndexAnnotation(new IndexAttribute("AK_Labour", 2) { IsUnique = true }));
+
+            Property(p => p.WorkOrderId)
+                .HasColumnAnnotation("Index",
+                    new IndexAnnotation(new IndexAttribute("AK_Labour", 1) { IsUnique = true }));
+
+            Property(p => p.ServiceItemName)
+                .HasMaxLength(80)
+                .IsRequired();
+
+            Property(p => p.LabourHours)
+                .HasPrecision(18, 2);
+
+            Property(p => p.Rate)
+               .HasPrecision(18, 2);
+
+            Property(p => p.ExtendedPrice)
+                .HasPrecision(18, 2)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+            Property(p => p.Notes)
+                .HasMaxLength(140)
+                .IsOptional();
+        }
+    }
+}
---
+++ DetailWorkflow/Models/Labor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DetailWorkflow.Models
+{
+    public class Labor
+    {
+        public int LaborId { get; set; }
+        public int WorkOrderId { get; set; }
+        public WorkOrder WorkOrder { get; set; }
+        public string ServiceItemCode { get; set; }
+        public string ServiceItemName { get; set; }
+        public decimal LabourHours { get; set; }
+        public decimal Rate { get; set; }
+        public decimal ExtendedPrice { get; set; }
+        public string Notes { get; set; }
+    }
+}
