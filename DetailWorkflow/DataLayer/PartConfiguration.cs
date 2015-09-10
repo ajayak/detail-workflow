1bc3023bde7169996cfe1913b4a29e06985b8658
Ajay Kumar <ajayak731@hotmail.com>
2015-09-10 21:36:40 +0530

    added Part and PartConfiguration

---
+++ DetailWorkflow/DataLayer/PartConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using DetailWorkflow.Models;
+
+namespace DetailWorkflow.DataLayer
+{
+    public class PartConfiguration : EntityTypeConfiguration<Part>
+    {
+        public PartConfiguration()
+        {
+            Property(p => p.InventoryItemCode)
+                .HasMaxLength(15)
+                .IsRequired()
+                .HasColumnAnnotation("Index",
+                new IndexAnnotation(new IndexAttribute("AK_Part", 2) { IsUnique = true }));
+
+            Property(p => p.WorkOrdeId)
+                .HasColumnAnnotation("Index",
+                    new IndexAnnotation(new IndexAttribute("AK_Part", 1) { IsUnique = true }));
+
+            Property(p => p.InventoryItemName)
+                .HasMaxLength(80)
+                .IsRequired();
+
+            Property(p => p.UnitPrice)
+                .HasPrecision(18, 2);
+
+
+            Property(p => p.ExtendedPrice)
+                .HasPrecision(18, 2)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+            Property(p => p.Notes)
+                .HasMaxLength(140)
+                .IsOptional();
+        }
+    }
+}
---
+++ DetailWorkflow/Models/Part.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DetailWorkflow.Models
+{
+    public class Part
+    {
+        public int PartId { get; set; }
+        public int WorkOrdeId { get; set; }
+        public WorkOrder WorkOrder { get; set; }
+        public string InventoryItemCode { get; set; }
+        public string InventoryItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ExtendedPrice { get; set; }
+        public string Notes { get; set; }
+        public bool IsInstalled { get; set; }
+    }
+}
