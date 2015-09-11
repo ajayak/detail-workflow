cdb21ffe6cf1d4b9b53e6e293e616e47f459a0ba
Ajay Kumar <ajayak731@hotmail.com>
2015-09-11 10:10:15 +0530

    Added dbset's and code first configuration

--- DetailWorkflow/Models/IdentityModels.cs
+++ DetailWorkflow/Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using DetailWorkflow.DataLayer;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -23,6 +24,27 @@ namespace DetailWorkflow.Models
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<Labor> Labors { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new InventoryItemConfiguration());
+            modelBuilder.Configurations.Add(new LabourConfiguration());
+            modelBuilder.Configurations.Add(new PartConfiguration());
+            modelBuilder.Configurations.Add(new ServiceItemConfiguration());
+            modelBuilder.Configurations.Add(new WorkOrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
         }
 
         public static ApplicationDbContext Create()
