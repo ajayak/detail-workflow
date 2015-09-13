b207d70d3d3f8fed5ed0eca5746ffa8c89a5798c
Ajay Kumar <ajayak731@hotmail.com>
2015-09-13 16:18:27 +0530

    Added ApplicationRolesController and updated CRUD operations using identity RoleManager

---
+++ DetailWorkflow/Controllers/ApplicationRolesController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DetailWorkflow.DataLayer;
+using DetailWorkflow.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace DetailWorkflow.Controllers
+{
+    public class ApplicationRolesController : Controller
+    {
+        public ApplicationRolesController()
+        {
+        }
+
+        public ApplicationRolesController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            set { _userManager = value; }
+        }
+
+        private ApplicationRoleManager _roleManager;
+
+        public ApplicationRoleManager RoleManager
+        {
+            get { return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>(); }
+            set { _roleManager = value; }
+        }
+
+        // GET: ApplicationRoles
+        public async Task<ActionResult> Index()
+        {
+            return View(await RoleManager.Roles.ToListAsync());
+        }
+
+        // GET: ApplicationRoles/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = await RoleManager.FindByIdAsync(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        //// GET: ApplicationRoles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ApplicationRoles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Name")] ApplicationRole applicationRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var roleResult = await RoleManager.CreateAsync(applicationRole);
+                if (!roleResult.Succeeded)
+                {
+                    ModelState.AddModelError("", roleResult.Errors.First());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(applicationRole);
+        }
+
+        //// GET: ApplicationRoles/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = await RoleManager.FindByIdAsync(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        //// POST: ApplicationRoles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] ApplicationRole applicationRole)
+        {
+            if (ModelState.IsValid)
+            {
+                var retrievedRole = await RoleManager.FindByIdAsync(applicationRole.Id);
+                var originalName = retrievedRole.Name;
+
+                if (originalName == "Admin" && applicationRole.Name.ToLower() != "admin")
+                {
+                    ModelState.AddModelError("", "You cannot change the name of Admin Role");
+                    return View(applicationRole);
+                }
+
+                if (originalName != "Admin" && applicationRole.Name.ToLower() == "admin")
+                {
+                    ModelState.AddModelError("", "You cannot change the name of a role to Admin");
+                    return View(applicationRole);
+                }
+
+                retrievedRole.Name = applicationRole.Name;
+                await RoleManager.UpdateAsync(retrievedRole);
+
+                return RedirectToAction("Index");
+            }
+            return View(applicationRole);
+        }
+
+        //// GET: ApplicationRoles/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = await RoleManager.FindByIdAsync(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
+
+        //// POST: ApplicationRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            var applicationRole = await RoleManager.FindByIdAsync(id);
+
+            if (applicationRole.Name == "Admin")
+            {
+                ModelState.AddModelError("", "You cannot delete Admin Role");
+                return View(applicationRole);
+            }
+
+            await RoleManager.DeleteAsync(applicationRole);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                RoleManager.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
